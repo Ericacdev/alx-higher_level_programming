86f33b0565ef17510a938deb57717d7e32544261
Ericacdev <nechipato@gmail.com>
2024-01-09 08:11:46 -0800

    edited file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,7 +2,7 @@
 """function that reads a text file (UTF8)"""
 
 
-def read_file(filename="):
+def read_file(filename=""):
 """Prints the UTF8 textfile content to stdout."""
 with open(filename, encoding="utf-8" as f:
 print(f.read(), end="")
