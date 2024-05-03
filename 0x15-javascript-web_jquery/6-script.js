ef3edcbf0ed3eb59df57b33bd081d8a3fb6b2bae
Ericacdev <nechipato@gmail.com>
2024-05-02 17:00:46 -0700

    updated files

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,5 @@
+V#update_header').click(function () {
+
+  $('HEADER').text('New Header!!!');
+
+});
---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,9 @@
+$('document').ready(function () {
+
+  $.get('https://hellosalut.stefanbohacek.dev/?lang=fr', function (data) {
+
+    $('DIV#hello').text(data.hello);
+
+  });
+
+});
