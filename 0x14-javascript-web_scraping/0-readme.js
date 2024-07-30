9dc792f4c5b61d1a7c5f7b39b27cfef43542bd40
Ericacdev <nechipato@gmail.com>
2024-07-30 16:58:50 -0700

    file 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,7 +2,4 @@
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', function (error, info) {
   console.log(error || info);
-})t fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, info) {
-  console.log(error || info);
 });
