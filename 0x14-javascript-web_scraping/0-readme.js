ed156e9fa85aab4ff6753105f27b9ce337c1c15b
Ericacdev <nechipato@gmail.com>
2024-07-30 16:42:39 -0700

    files 0-6

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', function (error, info) {
+  console.log(error || info);
+})t fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', function (error, info) {
+  console.log(error || info);
+});
---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const request = require('request');
+request(process.argv[2], (error, response, body) => {
+  if (!error) {
+    const todos = JSON.parse(body);
+    const finished = {};
+    todos.forEach((todo) => {
+      if (finished[todo.userId] === undefined && todo.completed) {
+        finished[todo.userId] = 1;
+      } else if (todo.completed) {
+        finished[todo.userId] += 1;
+      }
+    });
+    console.log(finished);
+  }
+});
+
